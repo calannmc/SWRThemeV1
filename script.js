ccfd0b1f39142a31b0deb8e653d34a989b454124
calannmc <info@techwitch.uk>
2024-09-02 22:07:05 +0100

    attachment

--- script.js
+++ script.js
@@ -18,15 +18,7 @@
   }
 
 
-  var ticketForm = location.search.split('ticket_form_id=')[1];
-  if(ticketForm == 18570) {
-    $('section.main-column h1').html('Product Registration');
-    $('#request_subject').val('Product Registration');
-    $('#request_description').val('There is a new product registration.');
-    $('#request_subject').parent('.request_subject').hide(); // Hide subject
-    $('#request_description').parent('.request_description').hide(); 
-    $("<p>Please upload your product receipt here.<p>").insertAfter('label:contains("Attachments")'); // Adds text below "Attachments"
-  }
+
 
   // Navigation
  
